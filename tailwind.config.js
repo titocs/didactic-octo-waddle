11e096ac554cdd08fca7e513e037cce852dd6f7b
= <titocs845@gmail.com>
2024-12-23 14:27:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*html", "index.html", ".html",],
+  theme: {
+    extend: {},
+		fontFamily: {
+			'main-font' : ['Ubuntu', 'sans-serif']
+		},
+  },
+  plugins: []
+}
